884388753182e2395b4d3a5e2f863be9167cb84c
Sergio Luis Mendoza Vasquez <smendoza@cotecmar.com>
2024-01-15 15:23:11 -0500

    Logre conectar Flask con Angular pero solo funciona la peticion get

--- angular-web-app/src/app/user/components/search-user/search-user.component.ts
+++ angular-web-app/src/app/user/components/search-user/search-user.component.ts
@@ -1,28 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../interfaces/user.interface';
+import { ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'search-user',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search-user.component.html',
   styleUrl: './search-user.component.css'
 })
 export class SearchUserComponent implements OnInit{
   
-  constructor(private serviceUser: UserService){}
+  constructor(private serviceUser: UserService, private formBuilder:FormBuilder){}
 
-  user?:User;
+  users?:User[];
+  form:FormGroup = this.formBuilder.group<User>({
+    name:'',
+    password:'',
+    username:''
+  });
   ngOnInit(): void {
     this.serviceUser.getUsers().subscribe(
       {
-        next: (u:User | undefined)=>{
+        next: (u:User[] | undefined)=>{
           console.log(u)
-          this.user = u;
+          this.users = u;
         },
         error:(err)=>{console.log(err)}
-      }
+      })
+                                      //<User>
+    this.form = this.formBuilder.group<User>({
+      name:'',
+      password:'',
+      username:''
+    })
+  }
+  enviarData(){
+    //this.form?.value
+    this.serviceUser.addUser({
+      name: this.form.value.name,
+      password: this.form.value.password,
+      username: this.form.value.username
+    }).subscribe(
+      respuesta =>{console.log("Comentario enviado");}
     )
   }  
 }
--- angular-web-app/src/app/user/services/user.service.ts
+++ angular-web-app/src/app/user/services/user.service.ts
@@ -9,10 +9,19 @@ import { User } from "../interfaces/user.interface";
 export class UserService{
     constructor(private http: HttpClient){ }
 
-    url:string = "http://127.0.0.1:5000/api/v1/users";
-    getUsers(): Observable<User | undefined>{
-        return this.http.get<User >(this.url).pipe(
+    // url:string = "http://127.0.0.1:5000/api/v1/users";
+    url:string = "http://127.0.0.1:5000/users";
+    getUsers(): Observable<User[] | undefined>{
+        return this.http.get<User[] >(this.url).pipe(
             catchError ((err)=>{
+                console.log(err)
+                return of(undefined)
+            })
+        )
+    }
+    addUser(body:User): Observable<User | undefined>{
+        return this.http.post<User>("http://127.0.0.1:5000/user",body).pipe(
+            catchError((err)=>{
                 console.log(err)
                 return of(undefined)
             })
